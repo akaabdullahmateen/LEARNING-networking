e7d52199c7e8acf12371d32b8c3a587676f5aa1e
akaAbdullahMateen <akaabdullahmateen@gmail.com>
2022-06-30 23:02:04 +0500

    Converted indentation to spaces

--- packets/ipv4_header.h
+++ packets/ipv4_header.h
@@ -6,24 +6,26 @@
 
 class Ipv4_header{
 private:
-	unsigned char dump[60];
-	std::map<std::string, std::string> header;
+    unsigned char dump[60];
+    std::map<std::string, std::string> header;
 
-	void default_initialize_dump();
-	void initialize_header();
-	unsigned int calculate_checksum();
-	std::string get_dscp_keyword(unsigned int n);
-	std::string get_ecn_keyword(unsigned int n);
-	std::string get_protocol_keyword(unsigned int n);
+    void default_initialize_dump();
+    void initialize_header();
+    unsigned int calculate_checksum();
+    unsigned int get_cidr(const std::string &ipv4_address_class);
+    std::string get_dscp_keyword(unsigned int n);
+    std::string get_ecn_keyword(unsigned int n);
+    std::string get_protocol_keyword(unsigned int n);
+    std::string get_subnet_mask(const std::string &ipv4_address_class);
 
 
 public:
-	Ipv4_header();
-	Ipv4_header(unsigned char *begin, unsigned char *end);
-	void display_blueprint();
-	void display_header();
-	void display_raw();
-	std::map<std::string, std::string> get_header();
+    Ipv4_header();
+    Ipv4_header(unsigned char *begin, unsigned char *end);
+    void display_blueprint();
+    void display_header();
+    void display_raw();
+    std::map<std::string, std::string> get_header();
 };
 
 #endif
