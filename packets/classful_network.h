9041a26c584f518da22b5e284badea7502564e9f
akaAbdullahMateen <akaabdullahmateen@gmail.com>
2022-06-30 23:01:48 +0500

    Converted indentation to spaces

--- packets/classful_network.h
+++ packets/classful_network.h
@@ -3,21 +3,21 @@
 
 class Classful_network{
 private:
-	std::string address;
-	unsigned int octets[4];
+    std::string address;
+    unsigned int octets[4];
 
-	void default_initialize();
-	bool is_octets_valid(const std::string &ipv4_address);
-	std::size_t get_dot_count(const std::string &ipv4_address);
-	bool is_length_valid(const std::string &ipv4_address);
-	bool is_characters_dot_or_digit(const std::string &ipv4_address);
-	bool is_address_valid(const std::string &ipv4_address);
+    void default_initialize();
+    bool is_octets_valid(const std::string &ipv4_address);
+    std::size_t get_dot_count(const std::string &ipv4_address);
+    bool is_length_valid(const std::string &ipv4_address);
+    bool is_characters_dot_or_digit(const std::string &ipv4_address);
+    bool is_address_valid(const std::string &ipv4_address);
 
 public:
-	Classful_network();
-	Classful_network(const std::string &ipv4_address);
-	void display_blueprint();
-	std::string get_address_class();
+    Classful_network();
+    Classful_network(const std::string &ipv4_address);
+    void display_blueprint();
+    std::string get_address_class();
 };
 
 #endif
